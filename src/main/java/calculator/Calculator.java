f510b97cd7bd8d09d739b7ed8b572c6e871e5aab
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2023-11-28 22:40:48 +0330

    Add function to Calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import static java.lang.Math.sqrt;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public double fract(int a, int b) { return sqrt((double) a / b); }
 }
